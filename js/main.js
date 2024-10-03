29fa4621f0241bd8ecdca197dd8f531da79f08c8
Sevinch <sevincha2008@gmail.com>
2024-10-03 14:59:55 +0500

    JS 2dars
    Zeromax IT-center 37group

---
+++ js/main.js
@@ -0,0 +1,84 @@
+//if            => agar
+//else          =>u holda
+//else if       => yoki bo'lmasa
+ 
+// if("salom" == "salom"){
+//     console.log("Ular teng!");
+// }
+// else{
+//     console.log("Ular teng emas")
+// }
+
+// if(44 >=45){
+//     console.log("Ular teng yoki katta")
+// }
+// else{
+//     console.log("Ular teng emas yoki Katta emas")
+// }
+
+// if("How are you" == "How do you do"){
+//     console.log("Ular birhil")
+// }
+// else{
+//     console.log("Ular xarhil")
+// }
+
+// if(46 <= 45){
+//     console.log("Solishtirayotgan son 45ga teng yoki katta")
+// }
+// else{
+//     console.log("Solishtiriyotgan son 45ga teng emas yoki katta ham emas")
+// }
+
+
+// if(51+49-50 <= 55+45-60){
+//     console.log("ular teng")
+// }
+// else{
+//     console.log("ular teng emas")
+// }
+
+// if(45 == 45){
+//     console.log("Ular teng")
+// }
+// else{
+//     console.log("Ular ten emas")
+// }
+
+// if(45==44){
+//     console.log("1 shart bajarildi")
+// }
+// else if(45 ==46){
+//     console.log("2 shart bajarildi")
+// }
+// else{
+//     console.log("Yuqoridagi shartlardan birontasi bajarilmadi")
+// }
+
+// let a = 45;
+
+// if(a >= 45){
+//     console.log("berilgan sondan kichik ypki teng");
+// }
+// else{
+//     console.log("a ning qiymati topilmadi");
+// }
+
+let a = 46;
+let b = 10;
+let c = 19;
+let d = 49;
+
+// if(a == 46 && b == 10 && c == 19 && d == 49){
+//     console.log("shartning hammasi to'g'ri");
+// }
+// else{
+//     console.log("shartlarning hammasi yoki bittasi noto'g'ri");
+// }
+
+// if(a == 46 || b == 10 || c == 19 || d == 49){
+//     console.log("Shartlarning kamida bittasi yoki hammasi to'g'ri");
+// }
+// else{
+//     console.log("Shartlarning hammasi noto'g'ri");
+// }
